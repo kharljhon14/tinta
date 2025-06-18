323d3102e0e4c84e32f8bf23b6199848983ba6da
karl <enriquezkharl14@gmail.com>
2025-06-18 10:04:53 +0800

    [Feat] write json helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
 
+	"maps"
+
 	"github.com/julienschmidt/httprouter"
 )
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Insert(w.Header(), maps.All(headers))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	w.Write(js)
+
+	return nil
+}
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
