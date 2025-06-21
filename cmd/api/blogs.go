bd6e5ccfc59c40a6ee25b0a7c981a41c5418447a
karl <enriquezkharl14@gmail.com>
2025-06-21 10:56:33 +0800

    [Feat] read json helper

--- cmd/api/blogs.go
+++ cmd/api/blogs.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -17,7 +16,7 @@ func (app *application) createBlogHandler(w http.ResponseWriter, r *http.Request
 		Tags    []string `json:"tags"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
