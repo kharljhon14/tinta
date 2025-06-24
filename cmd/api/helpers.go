deee1b8791980799d7c10e3c1333dece10d1ece2
karl <enriquezkharl14@gmail.com>
2025-06-24 23:07:25 +0800

    [Feat] query string helper functions

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"maps"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/kharljhon14/tinta/internal/validator"
 )
 
 type envelope map[string]any
@@ -98,4 +100,40 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 	return id, nil
 
+}
+
+func (app *application) readString(qs url.Values, key, defaultValue string) string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string {
+	csv := qs.Get(key)
+
+	if csv == "" {
+		return defaultValue
+	}
+
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be a integer value")
+		return defaultValue
+	}
+
+	return i
 }
