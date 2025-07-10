c85da53a697e987472da20450b0fa83f1ec226e9
karl <enriquezkharl14@gmail.com>
2025-07-10 10:37:51 +0800

    [Feat] permissions model

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,15 +10,17 @@ var (
 )
 
 type Models struct {
-	Blogs  BlogModel
-	Users  UserModel
-	Tokens TokenModel
+	Blogs       BlogModel
+	Users       UserModel
+	Tokens      TokenModel
+	Permissions PermissionModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Blogs:  BlogModel{DB: db},
-		Users:  UserModel{DB: db},
-		Tokens: TokenModel{DB: db},
+		Blogs:       BlogModel{DB: db},
+		Users:       UserModel{DB: db},
+		Tokens:      TokenModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
